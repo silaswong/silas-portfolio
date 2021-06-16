3714160722713e8435c29ad0de4b03e2cdd99c9a
silaswong <silas.wong@p2c.com>
2021-06-16 12:43:52 -0400

    Create hero.tsx

---
+++ src/@lekoarts/gatsby-theme-cara/src/components/hero.tsx
@@ -0,0 +1,30 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui"
+import Divider from "../elements/divider"
+import Inner from "../elements/inner"
+import Content from "../elements/content"
+import Svg from "./svg"
+import { UpDown, UpDownWide } from "../styles/animations"
+// @ts-ignore
+import Intro from "../sections/intro"
+
+const Hero = ({ offset, factor = 1 }: { offset: number; factor?: number }) => (
+  <div>
+    <Divider speed={0.2} offset={offset} factor={factor}>
+      <UpDown>
+  
+      </UpDown>
+      <UpDownWide>
+
+      </UpDownWide>
+  
+    </Divider>
+    <Content sx={{ variant: `texts.bigger` }} speed={0.4} offset={offset} factor={factor}>
+      <Inner>
+        <Intro />
+      </Inner>
+    </Content>
+  </div>
+)
+
+export default Hero
