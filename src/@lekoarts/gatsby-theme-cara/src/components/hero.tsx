c457122765f10f7aa794de27851db5f401b30072
Silas Wong <silas.wong@p2c.com>
2021-06-30 17:12:58 -0400

    Update hero.tsx

--- src/@lekoarts/gatsby-theme-cara/src/components/hero.tsx
+++ src/@lekoarts/gatsby-theme-cara/src/components/hero.tsx
@@ -12,12 +12,14 @@ const Hero = ({ offset, factor = 1 }: { offset: number; factor?: number }) => (
   <div>
     <Divider speed={0.2} offset={offset} factor={factor}>
       <UpDown>
-  
+        <Svg icon="triangle" hiddenMobile width={48} stroke color="icon_orange" left="10%" top="20%" />
+        <Svg icon="triangle" hiddenMobile width={48} stroke color="icon_orange" left="10%" top="20%" />
+        <Svg icon="triangle" hiddenMobile width={48} stroke color="icon_orange" left="10%" top="20%" />
       </UpDown>
       <UpDownWide>
-
+         <Svg icon="triangle" hiddenMobile width={48} stroke color="icon_orange" left="10%" top="20%" />
       </UpDownWide>
-  
+        <Svg icon="triangle" hiddenMobile width={48} stroke color="icon_orange" left="10%" top="20%" />
     </Divider>
     <Content sx={{ variant: `texts.bigger` }} speed={0.4} offset={offset} factor={factor}>
       <Inner>
